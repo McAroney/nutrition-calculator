1f70b6dbc1fe0b0219bef530c3b5d587bc0d9cd6
Aron Elal <aron.elal@gmail.com>
2019-12-07 13:20:21 +0200

    Back on track

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
-import { Provider } from './Context'
-import Search from './components/Search'
-import Resultset from './components/Resultset'
-import Recipe from './components/Recipe'
-import Firebase from './Firebase'
+import React, { Component } from "react";
+import { Provider } from "./Context";
+import Search from "./components/Search";
+import Resultset from "./components/Resultset";
+import Recipe from "./components/Recipe";
 
-import './App.css'
+import "./App.css";
 
 class App extends Component {
-  render () {
-    return (
-      <Firebase>
-        <Provider>
-          <div className='App'>
-            <header className='App-header'>
-              <h1>Nutricalc</h1>
-              <Search />
-              <Resultset />
-              <Recipe />
-            </header>
-          </div>
-        </Provider>
-      </Firebase>
-    )
-  }
+	render() {
+		return (
+			<Provider>
+				<div className="App">
+					<header className="App-header">
+						<h1>Nutricalc</h1>
+						<Search />
+						<Resultset />
+						<Recipe />
+					</header>
+				</div>
+			</Provider>
+		);
+	}
 }
 
-export default App
+export default App;
--- src/Context.js
+++ src/Context.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react'
-import reducer from './actions'
+import React, { Component } from "react";
+import reducer from "./actions";
 
-const Context = React.createContext()
+const Context = React.createContext();
 
 export class Provider extends Component {
-  state = {
-    ingridients: [],
-    recipe_message: '',
-    results: [],
-    recipe: [],
-    total_recipe_carbs: 0,
-    total_recipe_protein: 0,
-    total_recipe_sugar: 0,
-    total_recipe_fat: 0,
-    total_recipe_weight: 0,
-    dispatch: action => this.setState(state => reducer(state, action))
-  }
+	state = {
+		ingridients: [],
+		recipe_message: "",
+		results: [],
+		recipe: [],
+		total_recipe_carbs: 0,
+		total_recipe_protein: 0,
+		total_recipe_sugar: 0,
+		total_recipe_fat: 0,
+		total_recipe_weight: 0,
+		dispatch: action => this.setState(state => reducer(state, action))
+	};
 
-  render () {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    )
-  }
+	render() {
+		return (
+			<Context.Provider value={this.state}>
+				{this.props.children}
+			</Context.Provider>
+		);
+	}
 }
 
-export const Consumer = Context.Consumer
+export const Consumer = Context.Consumer;
